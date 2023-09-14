7acfe67989597432be9c7565094d4cf1bb49f4f2
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-09-14 23:10:05 +0300

    #4 added written to file with node.js

--- app/backend/index_db.js
+++ app/backend/index_db.js
@@ -1,16 +1,35 @@
 const mysql = require("mysql2");
+const fs = require('fs');
+
+let obj = {
+  table: []
+};
  
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
+  database: "dog_exhebition",
   password: "1234qseft"
 });
  
-connection.query("CREATE DATABASE usersdb2",
-  function(err, results) {
+// connection.query("CREATE DATABASE usersdb2",
+//   function(err, results) {
+//     if(err) console.log(err);
+//     else console.log("База данных создана");
+// });
+
+const sql = "SELECT * FROM auto";
+connection.query(sql,  function(err, results) {
     if(err) console.log(err);
-    else console.log("База данных создана");
+    const autos = results;
+    let new_json = JSON.stringify(autos)
+    fs.writeFile('auto.json', new_json, (err) => { 
+      if (err) throw err; 
+      console.log('Data written to file'); 
+      }); 
 });
+
+
  
 connection.end();
 // value="jdbc:mysql://localhost:3306/dog_exhebition" />
